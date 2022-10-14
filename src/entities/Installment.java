55e44e64ae11ff28f402662a180c6fc6977337fb
Moises Nantes <moisesnantes@gmail.com>
2022-10-13 21:47:29 -0400

    2ª dia de execicios

---
+++ src/entities/Installment.java
@@ -0,0 +1,45 @@
+package entities;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Installment {
+	
+	private LocalDate dueDate;
+	private Double amount;
+	
+	private List<Installment> installments = new ArrayList<>();
+	
+	
+	public Installment(LocalDate dueDate, Double amount) {
+		this.dueDate = dueDate;
+		this.amount = amount;
+	}
+
+
+	public LocalDate getDueDate() {
+		return dueDate;
+	}
+
+
+	public void setDueDate(LocalDate dueDate) {
+		this.dueDate = dueDate;
+	}
+
+
+	public Double getAmount() {
+		return amount;
+	}
+
+
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+	
+	
+	
+	
+	
+
+}
