2473d5aefdeeec5d4140bf31b6c4857298ffda57
Moises Nantes <moisesnantes@gmail.com>
2022-10-19 20:12:16 -0400

    Conclusão execicio

--- src/application/Programam.java
+++ src/application/Programam.java
@@ -1,8 +1,45 @@
 package application;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+import entities.Contract;
+import entities.Installment;
+import services.ContractService;
+import services.PaypalService;
+
 public class Programam {
 
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner (System.in);
+		
+		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		
+		System.out.println("Entre com  os dados do contrato: ");
+		System.out.print("Número: ");
+		int number = sc.nextInt();
+		System.out.println("Data (dd/MM/yyyy): ");
+		LocalDate date = LocalDate.parse(sc.next(), fmt);
+		System.out.println("Valor do Contrato: ");
+		double totalValue = sc.nextDouble();
+		
+		Contract obj = new Contract(number, date, totalValue);
+		
+		System.out.print("Entre com o numero de parcelas: ");
+		int n = sc.nextInt();
+		
+		ContractService contractService = new ContractService(new PaypalService());
+		
+		contractService.processContract(obj, n);
+		
+		System.out.println("Parcelas: ");
+		for(Installment installment : obj.getInstallments()) {
+			System.out.println(installment);
+		}
+		
+		sc.close();
 
 
 	}
